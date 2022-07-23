d77e6848caec317c988521ae359cebc20603a1c5
eamingk <eamingk@gmail.com>
2022-07-23 23:55:15 +1200

    globe stuff

--- globe/labels.js
+++ globe/labels.js
@@ -3,11 +3,11 @@ let labels = {
     "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 
     "features": [
-        { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[-138, 5.5],[-130.8, 3.5],[-120.8, 1],[-115.6, 0]] },
-            "properties": { "label": "Cities of the Demon Lords" }},
-        { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[-121, 26.25],[-102,7],[-85,-22]] },
-            "properties": { "label": "Haganna Peskudaz" }},
-        { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[-170, 25],[-154.5, 2.5],[-135,-16]] },
-            "properties": { "label": "The Opal Ocean" }}
+        { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[155, 15],[180,9],[-140, -8],[-105,-25]] },
+            "properties": { "label": "Pacific Ocean" }},
+        { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[-46,24],[-25,0],[-7,-30]] },
+            "properties": { "label": "Atlantic Ocean" }},
+        { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[56,-4],[98,-15]] },
+            "properties": { "label": "Indian Ocean" }}
     ]
 };
